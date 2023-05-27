34bbc20cc9a96c8f1916a22987478557b4f249d6
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-05-27 16:14:49 +0300

    completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        matched_words = []
+        sorted_word = sorted(self.word.lower())
+        for word in words:
+            if sorted(word.lower()) == sorted_word and word.lower() != self.word.lower():
+                matched_words.append(word)
+        return matched_words
